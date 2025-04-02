b8b70796f3d6bf9c05458dcf63c3c1e6bfef56b2
kwame-Owusu <alfredowusu595@gmail.com>
2025-04-02 14:37:36 +0100

    added files to unit test the textnode class behaviour

---
+++ src/test_textnode.py
@@ -0,0 +1,24 @@
+import unittest
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        expected = "TextNode('This is a text node', TextType.BOLD, None)"
+        self.assertEqual(repr(node), expected)
+
+    def test_not_equal_nodes(self): 
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.LINK)
+        self.assertNotEqual(node, node2)
+
+
+if __name__ == "__main__":
+    unittest.main()
+    
