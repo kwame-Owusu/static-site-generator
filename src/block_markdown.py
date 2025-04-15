72124c0ac85d144d5147c239d69481a90eb6a4e2
kwame-Owusu <alfredowusu595@gmail.com>
2025-04-15 17:18:36 +0100

    now test are passing and implementation complete for first block markdown function

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -4,23 +4,12 @@ import re
 
 
 
-def markdown_to_blocks(markdown):
-  markdown = markdown.strip()
-  blocks = []
-  splitted = markdown.split('\n\n')
-  for line in splitted:
-    if line.strip():
-      blocks.append(line.strip())
-  return blocks 
-
-md = """
-    This is **bolded** paragraph
-
-    This is another paragraph with _italic_ text and `code` here
-    This is the same paragraph on a new line
-
-    - This is a list
-    - with items
-    """
-
-print(markdown_to_blocks(md))
+def markdown_to_blocks(markdown: str) -> list[str]:
+  blocks = markdown.split("\n\n")
+  filtered_blocks = []
+  for block in blocks:
+    if block == "":
+      continue
+    block = block.strip()
+    filtered_blocks.append(block)
+  return filtered_blocks
